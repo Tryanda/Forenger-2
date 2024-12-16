583af9eb5992247dd236f8217dbff1524d0f361c
richovictor <victormathar4@gmail.com>
2024-12-17 00:30:13 +0700

    exitfuncadded

--- karakter_main_menu.gd
+++ karakter_main_menu.gd
@@ -3,4 +3,7 @@ extends CharacterBody3D
 @onready var animation = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
-	animation.play('walk')
+	animation.play('idle') 
+
+func _on_texture_button_pressed() -> void:
+	get_tree().quit() 
